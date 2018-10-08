a1ff62db413d9308f5b5aebb6a8236e7f44e250c
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2018-10-08 19:27:34 -0300

    Lista de exercício 01 - JavaScript adicionada.

---
+++ Lista de Exercícios 01 - JavaScript/js/Exer08JS.js
@@ -0,0 +1,8 @@
+
+var btn = document.getElementById("btn");
+var campo = document.getElementById("fieldText");
+
+btn.addEventListener("click", function() {
+    // Alerta o valor do campo
+    alert(campo.value);
+});
